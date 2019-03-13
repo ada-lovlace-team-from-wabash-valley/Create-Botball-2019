1070a7b7596d5937ef7c00345e10d9fa70a91866
DavidMutchler <mutchler@rose-hulman.edu>
2019-03-13 15:32:39 -0400

    Deleting files, preparing to put back library from last year.

--- include/create_library.h
+++
@@ -1,5 +0,0 @@
-#define TRUE 1
-#define FALSE 0
-
-void Sweepy_Thingie(int power, int servo);
-void servo(int port, int desired_position);
--- include/variables.h
+++
@@ -1,8 +0,0 @@
-#define claw_up 1065
-#define claw_water_cube 1868
-
-#define sweeper_open 1297
-#define sweeper_closed 0
-
-#define TRUE 1
-#define FALSE 0 
